5b697ae5e873d4b76306b48460b5b85d36d92a9e
Aditya Dimri <55507565+AdityaDimri1@users.noreply.github.com>
2025-08-05 15:03:16 +0530

    Add files via upload

---
+++ Binary_Search/problem_16.py
@@ -0,0 +1,21 @@
+def threeSumClosest(nums: list, target: int):
+    nums.sort()
+    n = len(nums)
+    closest_sum = float('inf')
+    
+    for i in range(n - 2):
+        left, right = i + 1, n - 1
+        while left < right:
+            current_sum = nums[i] + nums[left] + nums[right]
+            if abs(current_sum - target) < abs(closest_sum - target):
+                closest_sum = current_sum
+                
+            if current_sum < target:
+                left += 1
+            elif current_sum > target:
+                right -= 1
+            else:
+                return current_sum
+    return closest_sum
+
+threeSumClosest([-1, 2, 1, -4], 1)
